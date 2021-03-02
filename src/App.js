246b263e12f68fe0407f4c5f7d92bf3cf517bfd8
Pretty Sapna Tirkey <prettysapnatirkey@Bharats-MacBook-Pro.local>
2021-03-02 23:54:27 +0530

    code cleanup v2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component} from 'react';
-import TodoList from './modules/TodoList'
+import React, { Component } from "react";
+import TodoList from "./modules/TodoList";
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     // this.state= {
@@ -10,15 +10,13 @@ class App extends Component {
     // }
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
-        <TodoList name={"Pretty's List"}/>
+        <TodoList name={"Pretty's List"} />
       </div>
     );
   }
 }
 
 export default App;
-
-/**reference: https://www.youtube.com/watch?v=e_ZibOe77yo */
